3e0dbcaa0f2bcf9ecc34eabd3a2fc06a233d06bf
Jesus Peralta <desarrolladorwebodm@gmail.com>
2019-10-17 13:55:32 -0500

    modificaciones de Iphone x +

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
+const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -45,6 +45,9 @@ self.addEventListener('activate',e=>{
 	const respuesta = caches.keys().then(keys=>{
 		keys.forEach(key => {
 			if(key !== STATIC_CACHE && key.includes('static')){
+				return caches.delete(key);
+			}
+			if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
 				return caches.delete(key);
 			}
 		})
